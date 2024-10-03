8f7784ba706dad2fb34bd0858717fd9229678922
souloppen <arieloppenheimer@outlook.cl>
2024-10-03 20:59:54 -0300

    add:not found

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/HeroesView.vue"),
     },
+    {
+      path: "/:catchAll(.*)*",
+      name: "NotFound",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/NotFoundView.vue"),
+    },
   ],
 });
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+    <VCard class="w-50 my-5 mx-auto text-center bg-transparent">
+        <VCardTitle>
+            Not Found
+        </VCardTitle>
+        <VCardText>
+            Regresa al home
+        </VCardText>
+        <VCardActions>
+            <VBtn to="/" class="bg-blue-darken-2 mx-auto w-50">
+                Home
+            </VBtn>
+        </VCardActions>
+    </VCard>
+</template>
